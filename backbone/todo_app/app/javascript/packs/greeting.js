a0f00804fa890bd1c81c9e26642ecd0e383e8f96
ayokinya <ayokinya@gmail.com>
2020-10-19 16:41:57 +0900

    LEARN: add javascript to rails index.html.erb

---
+++ backbone/todo_app/app/javascript/packs/greeting.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+import { hello } from './hello';
+
+function greet_user(last_name, first_name) {
+  hello(last_name + " " + first_name);
+}
+// $(document).ready(function() {
+//   $('button#greet-user-button').on(
+//     'click',
+//     function() {
+//       greet_user('Dire', 'Strait');
+//     }
+//   );
+// });
+/* Or the ES6 version for this: */
+$(() =>
+  $('button#greet-user-button').on('click', () => greet_user('Dire', 'Strait'))
+);
+
