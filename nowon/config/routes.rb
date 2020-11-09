700e25ba5e79ef61d586c513889abc0f798310fe
ayokinya <ayokinya@gmail.com>
2020-11-09 12:49:19 +0900

    ADD: nowon set done

---
+++ nowon/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+  resources :messages
+  devise_for :users
+  resources :rooms
+  # resources :users
+  root 'welcome#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
