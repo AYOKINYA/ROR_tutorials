17a9b739ca28e9c7573bf0fa7c93b17272cef229
ayokinya <ayokinya@gmail.com>
2020-11-09 17:18:23 +0900

    ADD: users_list BACKBONE

--- nowon/app/javascript/packs/application.js
+++ nowon/app/javascript/packs/application.js
@@ -8,6 +8,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+const $ = require("jquery");
+require("backbone")
+import './users_list.js'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
