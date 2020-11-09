f495e5f37d51782cf0aa5104e22f8b2e83a451a3
ayokinya <ayokinya@gmail.com>
2020-11-09 19:54:39 +0900

    ADD: search username controller

--- nowon/app/controllers/users_controller.rb
+++ nowon/app/controllers/users_controller.rb
@@ -11,4 +11,14 @@ class UsersController < ApplicationController
     @users = User.all
     @user = User.find(params[:id])
   end
+
+  def search
+    if params[:search].blank?
+       redirect_to users_path and return
+    else
+        @parameter = params[:search].downcase
+        @results = User.all.where("lower(username) LIKE :search", search: @parameter)
+    end
+    render 'index'
+  end
 end
