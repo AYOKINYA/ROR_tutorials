bab9d91fc585b49639d3c2b9c17735f20e822624
ayokinya <ayokinya@gmail.com>
2020-11-10 16:03:52 +0900

    WORKING: users_search.js

--- nowon/app/javascript/packs/users_search.js
+++ nowon/app/javascript/packs/users_search.js
@@ -3,65 +3,86 @@ import _ from "underscore"
 import Backbone from 'backbone';
 
 _.templateSettings = {
-	interpolate : /\{\{=(.+?)\}\}/g,
-	escape : /\{\{-(.+?)\}\}/g,
-	evaluate: /\{\{(.+?)\}\}/g,
+    interpolate : /\{\{=(.+?)\}\}/g,
+    escape : /\{\{-(.+?)\}\}/g,
+    evaluate: /\{\{(.+?)\}\}/g,
   };
 
 $(function() {
 
-	var SearchedUser = Backbone.Model.extend({
-		urlRoot: '/search.json'
+  var SearchModel = Backbone.Model.extend({
+    urlRoot: '/users'
+  });
+  
+  var SearchCollection = Backbone.Collection.extend({
+    model: SearchModel,
+    url: '/users.json'
+  });
+
+  var SearchListItemView = Backbone.View.extend({
+    //tagName: 'li',
+    className: 'search',
+    template: _.template($('#search-item-tmpl').html()),
+    
+  
+    initialize: function() {
+      this.listenTo(this.model, 'destroy', this.remove)
+    },
+  
+    render: function() {
+      var html = this.template(this.model.toJSON());
+      this.$el.html(html);
+      
+      console.log(this.model.get("username"));
+
+      return (this);
+    },
+  
+    events: {
+      'click .remove': 'onRemove'
+    },
+  
+    onRemove: function() {
+      this.model.destroy();
+    }
+  });
+  
+  var SearchListView = Backbone.View.extend({
+    el: '#search-app',
+  
+    initialize: function() {
+	  this.collection = new SearchCollection;
+	  this.listenTo(this.collection, 'sync', this.render);
+	  this.collection.fetch();	  
+	},
+	
+	events: {
+		'click .search' : 'search_user'
+	},
+  
+    render: function() {
+      var $list = this.$('.search-list').empty();
+  
+      this.collection.each(function(model) {
+        var item = new SearchListItemView({model: model});
+        $list.append(item.render().$el);
+      }, this);
+  
+      return this;
+	},
+	
+	search_user: function(e) {
+		e.preventDefault();
+		console.log("=======");
+		const name = this.$('#username-input').val();
+		console.log(name);
+		console.log("=======");
+		var found = _.find(this.collection.models, function(item){
+			return item.get('username') === name;
 		});
-
-	const SearchedUsers = Backbone.Collection.extend({
-		model: SearchedUser,
-		url: '/search.json'
-	});
-
-	const SearchedUsersView = Backbone.View.extend({
-
-		template: _.template($('#searched-user-item-tmpl').html()),
-		render: function () {
-			this.$el.html(this.template(this.model.toJSON()));
-			return this ;
-		},
-		initialize: function() {
-			this.model.on('change', this.render, this);
-		},
-		});
-
-	var SearchView = Backbone.View.extend({
-		el: $("#view-searched-users"),
-		
-		initialize: function () {
-			this.collection.on('add', this.addAll, this);
-			this.collection.on('reset', this.addAll, this);
-			this.collection.fetch();
-		  },
-		events: {
-		'submit' : 'search_users'
-		},
-		search_users: function () {
-			const username = $("#search-user-name").val();
-			console.log("heeee");
-			console.log(username);
-			if (!username)
-				return ;
-		},
-		addOne: function(user){
-			var view = new Chat.MessageView({model: user});
-			$('#searched-user-list').append(view.render().el);
-		},
-		addAll: function(){
-			this.$('#searched-user-list').html('');
-			this.collection.each(this.addOne, this);
-		},
-		render: function() {
-			this.addAll
-		}
-	});
-	  
-	  const searchedusers = new SearchedUsers;
-	  var SearchingView = new SearchView({collection: searchedusers});
+		found.fetch();
+	},
+  
+  });
+    var SearchView = new SearchListView;
 });
