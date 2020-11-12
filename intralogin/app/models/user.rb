7ec0fa8c544d7eefc772af621022783be1d768ac
ayokinya <ayokinya@gmail.com>
2020-11-12 16:20:49 +0900

    ADD: avatar_url from intra

--- intralogin/app/models/user.rb
+++ intralogin/app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.nickname = auth.info.nickname
+      user.avatar_url = auth.info.image
     end
   end
 end
