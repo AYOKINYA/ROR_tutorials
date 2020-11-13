ee9e4de106eb31f38ba2a715be203d42ecb65f52
ayokinya <ayokinya@gmail.com>
2020-11-13 18:02:10 +0900

    ADD: intra + 2-factor login

---
+++ intralogin/app/controllers/otp_auths_controller.rb
@@ -0,0 +1,21 @@
+class OtpAuthsController < ApplicationController
+
+    def create
+        @user = current_user
+        puts @user.name
+        otp_code = params["user"]["otp_attempt"]
+        ret = (current_user.validate_and_consume_otp!(otp_code) || current_user.invalidate_otp_backup_code!(params[:otp_attempt]))
+        user_session["otp_done"] = ret
+        render 'users/index'
+    end
+
+    def update
+        if (params[:id] == current_user[:id])
+            if (params[:otp_required_for_login] == "true")
+                puts "hi"
+            else
+                puts "hey"
+            end
+        end
+    end
+end
--- intralogin/config/routes.rb
+++ intralogin/config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session_path
   end
   resource :two_factor_settings, except: [:index, :show]
+  resource :otp_auth, except: [:index, :show]
   root 'users#index'
 end
